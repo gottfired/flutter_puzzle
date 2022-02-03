d4da28e9fd7d51db4c3ad4baa612bb151eff0dab
Gottfried Chen <gottfried.chen@allaboutapps.at>
2022-02-03 09:17:22 +0100

    improved anim

--- lib/particle.dart
+++ lib/particle.dart
@@ -6,33 +6,14 @@ import 'package:flutter/material.dart' hide Colors;
 class Particle {
   Vector3 position = Vector3.zero();
   Vector3 originalPosition = Vector3.zero();
+  double originalDistance = 0.0;
   Vector3 targetPosition = Vector3.zero();
   Vector4 color = Vector4.zero();
   double size = 0.0;
+}
 
-  draw2d(Canvas canvas, Size psSize, Size canvasSize, double time, Matrix3 transform) {
-    Paint paint = Paint()
-      ..color = Color.fromRGBO(
-        (color.x * 255).toInt(),
-        (color.y * 255).toInt(),
-        (color.z * 255).toInt(),
-        (position.z + 1) / 2,
-      )
-      ..strokeWidth = size;
-    // canvas.drawCircle(Offset(position.x, position.y), size, paint);
-
-    final maxSide = max(canvasSize.width, canvasSize.height) * 1.3;
-
-    final distance = position.distanceTo(Vector3.zero());
-    final rot = Matrix3.rotationZ(time * distance * 0.00001);
-    final pos = transform.transformed(rot.transformed(position));
-
-    double x = (pos.x * maxSide) / psSize.width + (canvasSize.width / 2);
-    double y = (pos.y * maxSide) / psSize.height + (canvasSize.height / 2);
-    double offset = 1 / (pos.z * 0.8 + 1.1);
-    double radius = size * 0.55 * offset * maxSide / psSize.width;
-    canvas.drawCircle(Offset(x, y), radius, paint);
-  }
+double wrap(double x, double min, double max) {
+  return x - (max - min) * (x / (max - min)).floor();
 }
 
 class ParticleSystem {
@@ -48,10 +29,33 @@ class ParticleSystem {
   }
 
   void draw2d(Canvas canvas, Size size) {
-    final rot = Matrix3.rotationZ(currentTime * 0.05);
+    final maxSide = max(size.width, size.height) * 1.3;
+    final centerX = size.width / 2;
+    final centerY = size.height / 2;
+    final factorX = maxSide / width;
+    final factorY = maxSide / height;
 
-    for (var particle in particles) {
-      particle.draw2d(canvas, Size(width, height), size, currentTime, rot);
+    final timeSwing = sin(currentTime * 0.3) * 0.0015;
+
+    bool first = true;
+    for (var p in particles) {
+      Paint paint = Paint()
+        ..color = Color.fromRGBO(
+          (p.color.x * 255).toInt(),
+          (p.color.y * 255).toInt(),
+          (p.color.z * 255).toInt(),
+          (p.position.z + 1) / 2,
+        );
+
+      final angle = timeSwing * p.originalDistance;
+      final rot = Quaternion.euler(0, 0, angle);
+      final pos = rot.rotated(p.position);
+
+      double x = pos.x * factorX + centerX;
+      double y = pos.y * factorY + centerY;
+      double offset = (0.55 * factorX) / (pos.z * 0.8 + 1.1);
+      double radius = p.size * offset;
+      canvas.drawCircle(Offset(x, y), radius, paint);
     }
   }
 
@@ -71,6 +75,7 @@ class ParticleSystem {
         particle.targetPosition.y = y;
         particle.originalPosition.x = x;
         particle.originalPosition.y = y;
+        particle.originalDistance = particle.position.distanceTo(Vector3.zero());
         particle.color = Colors.red;
         particle.size = 50.0;
 
@@ -85,11 +90,9 @@ class ParticleSystem {
   void tick(double time) {
     currentTime = time;
     for (var particle in particles) {
-      double distance = particle.position.distanceTo(Vector3.zero());
-
-      particle.position.z = 0.3 * sin(5 * time + particle.position.x * 0.3 + particle.position.y * 0.1);
       particle.position.x = particle.originalPosition.x + cos(time) * 30;
       particle.position.y = particle.originalPosition.y + sin(time) * 100;
+      particle.position.z = 0.3 * sin(5 * time + particle.position.x * 0.3 + particle.position.y * 0.1);
     }
   }
 }
