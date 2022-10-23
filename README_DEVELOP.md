20e161eefc5fba4770af42e42c6b22f9a89d15c5
Gottfried Chen <gottfried@chengine.com>
2022-10-23 12:55:16 +0200

    comments

---
+++ README_DEVELOP.md
@@ -0,0 +1,3 @@
+- add windows support `flutter create --platforms=windows .`
+- `flutter run` to start or play button in VScode debug pane
+    - play button adds hot reload
--- lib/scene.dart
+++ lib/scene.dart
@@ -6,6 +6,10 @@ enum SceneState {
   done,
 }
 
+// Base class for background scenes
+// A scene switches to the next one by entering
+// the fadeOut and then done state. Once it is done
+// the scene gets removed
 abstract class Scene {
   void tick();
   void render(Canvas canvas, Size size);
